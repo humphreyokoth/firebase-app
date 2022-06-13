6e51c0ca609b74f0ccc19aafe9fb7dc497b1d229
humphreyokoth <humpasp@gmail.com>
2022-06-13 21:12:24 +0300

    Home pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,48 +1,17 @@
 import React, { useState } from 'react'
-import { postData } from '../Services/functionServices';
 
-export default function Home() {
-    const [inputs, setInputs] = useState({});
-
-    const handleChange = (event) => {
-      const name = event.target.name;
-      const value = event.target.value;
-      setInputs(prev => ({...prev, [name]: value}))
-    }
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      console.log(inputs)
-    postData("todos",inputs)
-    }
-  return (
-    <form onSubmit={handleSubmit} className="d-flex flex-column bd-highlight mb-3 container mt-5">
-      <label>Title:
-      <input 
-        type="text" 
-        name="todo" 
-        value={inputs.todo || ""} 
-        onChange={handleChange}
-      />
-      </label>
-      <label>Description:
-      <input 
-        type="text" 
-        name="todo" 
-        value={inputs.todo || ""} 
-        onChange={handleChange}
-      />
-      </label>
-      <label>Author:
-      <input 
-        type="text" 
-        name="todo" 
-        value={inputs.todo || ""} 
-        onChange={handleChange}
-      />
-      </label>
+const Home = () => {
+    const [inputs,setInputs]  = useState({});
     
-        <input type="submit" />
-    </form>
+    const handleChange = (e)=>{
+        const name = e.target.name;
+        const value = e.target.value;
+        setInputs(prev=>({...prev,[name]:value}))
+    }
+    
+  return (
+    <form>Home</form>
   )
 }
+
+export default Home
