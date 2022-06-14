e86c762900ad8fb4ccddc6ccb9bdf09fda698a6a
humphreyokoth <humpasp@gmail.com>
2022-06-14 09:27:25 +0300

    update firestore data

--- src/Services/functionServices.js
+++ src/Services/functionServices.js
@@ -1,14 +1,26 @@
 
-import { collection, addDoc } from "firebase/firestore"; 
+import { collection, addDoc,doc, updateDoc, } from "firebase/firestore"; 
 import { db } from "../Config/firebaseConfig";
+
+
+
+
+
+
+
 export async function postData(data){
     try {
         const docRef = await addDoc(collection(db, "todo"),data);
+        const documentRef =  await doc(db,"todo",docRef.id);
+        await updateDoc(documentRef,{
+          id:docRef.id
+        })
         console.log("Document written with ID: ", docRef.id);
       } catch (e) {
         console.error("Error adding document: ", e);
     }
 }
+
 
 
 // export async function postData(data){
